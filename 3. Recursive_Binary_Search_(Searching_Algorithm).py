586aff2ecdd9c0bf8b31ac29a3b734a572412785
WindJammer6 <98175995+WindJammer6@users.noreply.github.com>
2023-08-28 02:10:10 +0800

    Update 3. Recursive_Binary_Search_(Searching_Algorithm).py

--- 3. Recursive_Binary_Search_(Searching_Algorithm).py
+++ 3. Recursive_Binary_Search_(Searching_Algorithm).py
@@ -23,4 +23,4 @@ if __name__ == '__main__':
     nums_list = [4, 9, 11, 17, 21, 25, 29, 32, 38]
     num_to_find = 32
     index = recursive_binary_search(nums_list, num_to_find)
-    print(f"Number found at index {index} using Recursive Binary Search")
+    print(f"Number found at index {index} using recursive Binary Search")
