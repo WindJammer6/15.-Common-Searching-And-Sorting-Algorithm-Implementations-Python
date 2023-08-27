4ce91cbc217659e607e881a57c42b855821f0392
WindJammer6 <98175995+WindJammer6@users.noreply.github.com>
2023-08-28 02:08:28 +0800

    Update 1. Linear_Search_(Searching_Algorithm).py

--- 1. Linear_Search_(Searching_Algorithm).py
+++ 1. Linear_Search_(Searching_Algorithm).py
@@ -6,7 +6,7 @@ def linear_search(numbers_list, number_to_find):
     return -1
 
 
-  if __name__ == '__main__':
+if __name__ == '__main__':
     nums_list = [4, 9, 11, 17, 21, 25, 29, 32, 38]
     num_to_find = 32
     index = linear_search(nums_list, num_to_find)
