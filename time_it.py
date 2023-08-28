b070e5a796dcf03983ffc187cb494cddc80e6ac0
WindJammer6 <98175995+WindJammer6@users.noreply.github.com>
2023-08-29 02:02:06 +0800

    Update time_it.py

--- time_it.py
+++ time_it.py
@@ -1,11 +1,5 @@
-#This code is a copy from '4.4. measuring_time_taken_by_functions_with_a_decorator_and_explaining_why_
-#functions_in_Python_are_considered_first_class_objects' from the folder 
-#'Tutorial 12 - Binary Search (Searching Algorithm) and Linear Search (Searching Algorithm)' (when you
-#import a name the file's name apparently can't have special characters and must only have alphabets 
-#hence I couldn't number it and just gave it the name 'time_it')
-
-#This code is used (through 'import') in the file 
-#'3. improving_on_bubble_sort_inefficiency_1.py' and '4. improving_on_bubble_sort_inefficiency_2.py'
+#This time_it decorator is used in the 'comparing_the_time_complexity_of_the_searching_algorithms_and_searching_pythonically.py' and the
+'comparing_the_time_complexity_of_the_sorting_algorithms_and_sorting_pythonically.py' files
 
 import time
 
